7d02669b8e1b605077f21def249f09a8cfc838e1
Isapedro <isa.pedro92@hotmail.com>
2024-01-16 19:50:04 +0100

    started js

---
+++ script.js
@@ -0,0 +1,25 @@
+const scoreCounter = document.querySelector(".score-counter");
+
+const grid = document.querySelector(".grid");
+
+const endGameScreen = document.querySelector(".end-game-screen");
+
+const endGameText = document.querySelector(".end-game-text");
+
+const playAgainButton = document.querySelector(".play-again");
+
+const totalCells = 100;
+const totalBombs = 90;
+const maxScore = 5;
+const bombsList=[];
+
+let score = 0;
+
+const cell =document.createElement("div");
+cell.classList.add("cell");
+
+for () {
+
+
+}
+
