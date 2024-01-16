33010e3de656ec34b90c66c321d901d6359cb585
Isapedro <isa.pedro92@hotmail.com>
2024-01-16 20:35:16 +0100

    addind score and boms around

--- script.js
+++ script.js
@@ -1,19 +1,22 @@
 const scoreCounter = document.querySelector(".score-counter");
-
 const grid = document.querySelector(".grid");
-
 const endGameScreen = document.querySelector(".end-game-screen");
-
 const endGameText = document.querySelector(".end-game-text");
-
 const playAgainButton = document.querySelector(".play-again");
-
 const totalCells = 100;
 const totalBombs = 90;
 const maxScore = 5;
 const bombsList=[];
 
 let score = 0;
+
+function updateScore(){
+score++;
+scoreCounter.innerText = score.toString().padStart(5, "0");
+if (score === maxScore)
+{
+  endGame();
+}}
 
 const cell =document.createElement("div");
 cell.classList.add("cell");
@@ -22,6 +25,35 @@ for (let i = 1; i<=100; i++) {
 const cell = document.createElement("div");
 cell.classList.add("cell");
 
+cell.addEventListener("click", function () {
+  if(bombsList.includes(i)){
+    cell.classList.add("cell-bomb");
+    endGame()
+
+  }
+cell.classList.add("cell-clicked");
+
+updateScore();
+});
+
 grid.appendChild(cell)
 }
 
+
+while (bombsList.length < totalBombs) {
+
+  const randomNumber = Math.floor(Math.random()* totalCells) + 1;
+if (!bombsList.includes(randomNumber)){
+   bombsList.push("randomNumber")
+}
+}
+
+function endGame(){
+  endGameText.innerHTML = "You<br>WON";
+  endGameScreen.classList.add("win");
+  endGameScreen.classList.remove("hidden");
+}
+
+playAgainButton.addEventListener("click", function(){
+  window.location.reload();
+})
